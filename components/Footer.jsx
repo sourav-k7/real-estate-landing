601d39f9ce1b19003d571a692a530cf1a0133041
sourav kumar <70101213+sourav-k7@users.noreply.github.com>
2024-10-04 17:49:45 +0530

    Footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,36 @@ import { Language } from "@mui/icons-material";
 import { IconButton } from "@mui/joy";
 import Image from "next/image";
 import Link from "next/link";
+import { FaFacebookF, FaGoogle, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 function Footer() {
+  let pages = [
+    "Home V1",
+    "Home V2",
+    "Home V3",
+    "About 1",
+    "About 2",
+    "Blog 1",
+    "Blog 2",
+    "Help Center 1",
+    "Help Center 2",
+    "FAQ V1",
+    "FAQ V2",
+    "FAQ Details",
+    "Contact V1",
+    "Contact V2",
+    "Checkout",
+  ];
+  let utilityPages = [
+    "Sign In",
+    "Sign Up",
+    "Forgot Password",
+    "Reset Password",
+    "Email Confirmation",
+    "404 Page",
+    "Coming Soon",
+    "Style Guide",
+  ];
   let socialLinks = [
     {
       url: "#",
@@ -11,19 +39,19 @@ function Footer() {
     },
     {
       url: "#",
-      icon: <Language className="text-white" sx={{ fontSize: 32 }} />,
+      icon: <FaFacebookF className="text-white" sx={{ fontSize: 32 }} />,
     },
     {
       url: "#",
-      icon: <Language className="text-white" sx={{ fontSize: 32 }} />,
+      icon: <FaTwitter className="text-white" sx={{ fontSize: 32 }} />,
     },
     {
       url: "#",
-      icon: <Language className="text-white" sx={{ fontSize: 32 }} />,
+      icon: <FaGoogle className="text-white" sx={{ fontSize: 32 }} />,
     },
     {
       url: "#",
-      icon: <Language className="text-white" sx={{ fontSize: 32 }} />,
+      icon: <FaLinkedinIn className="text-white" sx={{ fontSize: 32 }} />,
     },
   ];
   return (
@@ -35,12 +63,13 @@ function Footer() {
             alt="logo"
             width={147}
             height={50}
-            className="object-contain"
+            className="mb-3"
           />
         </Link>
         <span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam ut id
-          nisl tellus rhoncus, imperdiet
+          Lorem ipsum dolor sit amet, consectetur
+          <br /> adipiscing elit. Diam ut id nisl tellus
+          <br /> rhoncus, imperdiet
         </span>
         <div className="mt-8 flex flex-row gap-4">
           {socialLinks.map((opt, index) => (
@@ -49,14 +78,33 @@ function Footer() {
               aria-label="Open in new tab"
               component="a"
               href={opt.url}
-              className="bg-white bg-opacity-20 p-2 rounded-[10px]"
+              className="bg-white bg-opacity-20 p-2 rounded-[10px] h-12 w-12"
             >
               {opt.icon}
             </IconButton>
           ))}
         </div>
       </div>
-      <div>links</div>
+      <div>
+        <ul className="flex flex-row gap-40">
+          <li className="columns-2 gap-40">
+            <h2 className="font-bold text-lg mb-4">Pages</h2>
+            {pages.map((page) => (
+              <li key={page} className="mb-4">
+                {page}
+              </li>
+            ))}
+          </li>
+          <li>
+            <h2 className="font-bold text-lg mb-4">Utility Pages</h2>
+            {utilityPages.map((page) => (
+              <li key={page} className="mb-4">
+                {page}
+              </li>
+            ))}
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
