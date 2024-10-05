dc56331bde473e03cf54adaf9364beb765a84c9f
sourav kumar <70101213+sourav-k7@users.noreply.github.com>
2024-10-05 12:25:30 +0530

    hero section responsive done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
 import SearchSlug from "./SearchSlug";
 
 function Hero() {
   return (
-    <div className=" h-[420px] md:h-screen relative">
-      <Image src="/hero-img.png" alt="hero-image" fill />
-      <div className="absolute ml-5 mr-5 md:ml-10 md:mr-10 lg:ml-20 lg:mr-20 top-28 md:top-52">
-        <div className="font-bold text-white text-2xl lg:text-[64px] ">
+    <div
+      className="h-screen flex flex-col justify-center"
+      style={{
+        backgroundImage: "url('/hero-img.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="ml-5 mr-5 md:ml-10 md:mr-10 lg:ml-20 lg:mr-20">
+        <div className="font-bold text-white text-2xl lg:text-[64px] lg:leading-tight ">
           The Most Affordable <br /> Place To Stay
         </div>
         <SearchSlug />
--- components/SearchSlug.jsx
+++ components/SearchSlug.jsx
@@ -6,22 +6,25 @@ import { Search } from "@mui/icons-material";
 function SearchSlug() {
   let housingOptions = ["Lease", "Rent", "Buy"];
   return (
-    <div className="text-white mt-20 flex gap-2">
-      <SelectIndicator
-        defaultValue={housingOptions[0]}
-        options={housingOptions}
-        classes="w-28"
-      />
-      <Input
-        size="lg"
-        placeholder="Search Locality"
-        className="max-w-[350px] w-full"
-      />
+    <div className="text-white mt-20 flex flex-col sm:flex-row gap-3 sm:gap-2">
+      <div className="flex gap-2">
+        <SelectIndicator
+          defaultValue={housingOptions[0]}
+          options={housingOptions}
+          classes="text-xs sm:text-base w-28 sm:w-32"
+        />
+        <Input
+          size="lg"
+          placeholder="Search Locality"
+          className="text-xs sm:text-base w-full"
+        />
+      </div>
       <IconButton
         aria-label="Search"
-        className="bg-[#F4511E] px-[14px] py-[14px] hover:bg-[#F4511E] hidden sm:block"
+        className="bg-[#F4511E] px-[14px] py-[14px] hover:bg-[#F4511E] text-center"
       >
         <Search className="text-white" sx={{ fontSize: 24 }} />
+        <span className="text-white ml-3 sm:hidden">Search</span>
       </IconButton>
     </div>
   );
